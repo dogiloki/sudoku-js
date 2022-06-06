bd64dfc2da560847238c454f2f470dffac49df23
Julio Villanueva <wolfri2002@gmail.com>
2022-06-05 23:38:02 -0500

    Se cambio la clase Main por Consola y se agrego un if para saber si el sudoku tiene una solución

--- Consola.js
+++ Consola.js
@@ -1,4 +1,4 @@
-class Main{
+class Consola{
 
 	constructor(){
 		this.sudoku=new Sudoku();
@@ -7,7 +7,7 @@ class Main{
 
 	pedirNumero(){
 		let mensaje="Tamaño del tablero: "+Diccionario.num.tablero;
-		mensaje+="\n"+this.sudoku.mostrar(0);
+		mensaje+="\n"+this.sudoku.mostrar();
 		mensaje+="Ingrese una posición la fila, columna, número. Separados por comas";
 		let posicion=Util.entradaDato(mensaje,0).replaceAll(" ","").split(",");
 		let fila=(posicion[0]??0)-1;
@@ -16,7 +16,12 @@ class Main{
 		if(!this.sudoku.ponerNumero(fila,columna,numero)){
 			alert("La posición o número no es válido");
 		}
-		this.pedirNumero();
+		if(Sudoku.numeros<this.sudoku.tam_tablero){
+			this.pedirNumero();
+		}else{
+			alert("Juego terminado!!!");
+			this.sudoku.mostrar();
+		}
 	}
 
 }
